1baca920315b7a264b25afbbb839afd72694f913
Dominik Olczyk <dominik@cryptosphere-systems.com>
2021-03-27 00:52:48 +0100

    main

--- arduino_main.ino
+++ arduino_main.ino
@@ -10,7 +10,7 @@
 
 
 // GLOBALS
-const byte numReaders = 4;
+const byte numReaders = 3;
 
 const byte ssPins[] = {2,3,4};
 const byte resetPin = 8;
@@ -22,8 +22,19 @@ const String correctIDs[] = {"ASDASD","ASDASD","ASDASDASD"};
 
 String currentIDs[numReaders];
 
-const byte lockPin = AO;
+const byte lockPin = A0;
 
+
+
+void onSolve() {
+  #ifdef DEBUG
+  // Print debug info
+  Serial.println(F("Puzzle Solved!"));
+  #endif
+  // Release the lock
+  digitalWrite(lockPin, HIGH);
+}
+ 
 void setup() {
   // put your setup code here, to run once:
   #ifdef DEBUG
@@ -33,13 +44,13 @@ void setup() {
   #endif
 
   // Set lock pin as output and secure the lock.
-  pinMode(lockPin), OUTPUT);
-  digitalWrite(lockPin, HIGH);
+  pinMode(lockPin, OUTPUT);
+  digitalWrite(lockPin, LOW);
 
   // Initialize the SPI bus
   SPI.begin();
 
-  for uint8_t i=0; i<numReaders; i++) {
+  for(uint8_t i=0; i<numReaders; i++) {
      mfrc522[i].PCD_Init(ssPins[i], resetPin);
      // set antenna gain to max (check if works)
      //mfrc522[i].PCD_SetAntennaGain(MFRC522::PCD_RxGain::RxGain_max);
@@ -51,12 +62,12 @@ void setup() {
      Serial.print(String(ssPins[i]));
      Serial.print(F(". Antenna strength: "));
      Serial.print(mfrc522[i].PCD_GetAntennaGain());
-     Serial.print(F(". Version: ");
+     Serial.print(F(". Version: "));
      mfrc522[i].PCD_DumpVersionToSerial();
 
      delay(1000);
   }
-  Serial.println(F("---- END SETUP ----"))
+  Serial.println(F("---- END SETUP ----"));
 }
 
 void loop() {
@@ -71,8 +82,11 @@ void loop() {
     String readRFID = "";
     // If sensor detects a tag and is able to read it
     if(mfrc522[i].PICC_IsNewCardPresent() && mfrc522[i].PICC_ReadCardSerial()) {
-      readRFID = dump_byte_array(mfrc522[i].uid.uidByte, mfrc522[i].uid.size);
-    }
+//      readRFID = dump_byte_array(mfrc522[i].uid.uidByte, mfrc522[i].uid.size);
+      readRFID = "";
+      for ( uint8_t j = 0; j < 4; i++) { // The MIFARE PICCs that we use have 4 byte UID
+        readRFID.concat(String(mfrc522[i].uid.uidByte[j], HEX)); // Adds the 4 bytes in a single String variable
+      }
 
     if( readRFID != currentIDs[i]) {
       // Set flag to show that puzzle state has changed
@@ -95,7 +109,7 @@ void loop() {
   //IF the changedValue flag has been set, at least one sensor has changed;
   if(changedValue) {
     //Dump serial to the current state of all sensors
-    for uint8_t i=0; i<numReaders; i++) {
+    for(uint8_t i=0; i<numReaders; i++) {
       Serial.print(F("Reader #"));
       Serial.print(String(i));
       Serial.print(F(" on Pin #"));
@@ -106,16 +120,8 @@ void loop() {
     Serial.println(F("---"));
   }
   if(puzzleSolved) {
-    onSolve() //TODO
+    onSolve();
   }
   //add short delay before next polling sensors
   // delay(1000);
-}
-
-void onSolve() {
-  #ifdef DEBUG
-  // Print debug info
-  Serial.println(F("Puzzle Solved!"));
-   // Release the lock
-   digitalWrite(lockPin, LOW);
-}
+}}
